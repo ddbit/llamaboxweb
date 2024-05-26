bbc926047876806a9708f764605204a2f323c541
davide <davide@Mac-mini-di-davide.local>
2024-05-26 16:41:19 +0200

    new pricing from pipeline

---
+++ gpuprices/prices.js
@@ -0,0 +1,387 @@
+[
+    {
+        "GPU": "H100",
+        "hourly-price": 2.75,
+        "VRAM": 80,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "A100",
+        "hourly-price": 2.4,
+        "VRAM": 80,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "GH200",
+        "hourly-price": "Enquire for pricing",
+        "VRAM": 144,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "H100",
+        "hourly-price": 3.24,
+        "VRAM": 80,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "A100",
+        "hourly-price": 2.74,
+        "VRAM": 80,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "L40S",
+        "hourly-price": 1.96,
+        "VRAM": 48,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "V100/S",
+        "hourly-price": 0.83,
+        "VRAM": 16,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "A16",
+        "hourly-price": 0.54,
+        "VRAM": 16,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "Nvidia A100 (40GB)",
+        "hourly-price": "4.14",
+        "VRAM": "40",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "2x Nvidia A100 (40GB)",
+        "hourly-price": "8.28",
+        "VRAM": "80",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "4x Nvidia A100 (40GB)",
+        "hourly-price": "16.56",
+        "VRAM": "160",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "Nvidia A100 (80GB)",
+        "hourly-price": "5.04",
+        "VRAM": "80",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "2x Nvidia A100 (80GB)",
+        "hourly-price": "10.08",
+        "VRAM": "160",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "4x Nvidia A100 (80GB)",
+        "hourly-price": "20.16",
+        "VRAM": "320",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "8x Nvidia A100 (80GB)",
+        "hourly-price": "40.32",
+        "VRAM": "640",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "Nvidia A40 (Large)",
+        "hourly-price": "2.61",
+        "VRAM": "48",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "2x Nvidia A40 (Large)",
+        "hourly-price": "5.22",
+        "VRAM": "96",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "4x Nvidia A40 (Large)",
+        "hourly-price": "10.44",
+        "VRAM": "192",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "8x Nvidia A40 (Large)",
+        "hourly-price": "20.88",
+        "VRAM": "384",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "Nvidia A40",
+        "hourly-price": "2.07",
+        "VRAM": "48",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "Nvidia T4",
+        "hourly-price": "0.81",
+        "VRAM": "16",
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "8x NVIDIA H100 SXM",
+        "hourly-price": 27.92,
+        "VRAM": 80,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "1x NVIDIA H100 PCIe",
+        "hourly-price": 2.49,
+        "VRAM": 80,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "8x NVIDIA A100 SXM",
+        "hourly-price": 14.32,
+        "VRAM": 80,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "8x NVIDIA A100 SXM",
+        "hourly-price": 10.32,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "1x NVIDIA A100 SXM",
+        "hourly-price": 1.29,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "4x NVIDIA A100 PCIe",
+        "hourly-price": 5.16,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "2x NVIDIA A100 PCIe",
+        "hourly-price": 2.58,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "1x NVIDIA A100 PCIe",
+        "hourly-price": 1.29,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "1x NVIDIA A10",
+        "hourly-price": 0.75,
+        "VRAM": 24,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "4x NVIDIA A6000",
+        "hourly-price": 3.2,
+        "VRAM": 48,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "2x NVIDIA A6000",
+        "hourly-price": 1.6,
+        "VRAM": 48,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "1x NVIDIA A6000",
+        "hourly-price": 0.8,
+        "VRAM": 48,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "8x NVIDIA Tesla V100",
+        "hourly-price": 4.4,
+        "VRAM": 16,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "1x NVIDIA Quadro RTX 6000",
+        "hourly-price": 0.5,
+        "VRAM": 24,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "NVIDIA HGX H100",
+        "hourly-price": "4.76",
+        "VRAM": "80",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "NVIDIA H100 PCIe",
+        "hourly-price": "4.25",
+        "VRAM": "80",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A100 80GB NVLINK",
+        "hourly-price": "2.21",
+        "VRAM": "80",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A100 80GB PCIe",
+        "hourly-price": "2.21",
+        "VRAM": "80",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A100 40GB NVLINK",
+        "hourly-price": "2.06",
+        "VRAM": "40",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A100 40GB PCIe",
+        "hourly-price": "2.06",
+        "VRAM": "40",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A40",
+        "hourly-price": "1.28",
+        "VRAM": "48",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "RTX A6000",
+        "hourly-price": "1.28",
+        "VRAM": "48",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "RTX A5000",
+        "hourly-price": "0.77",
+        "VRAM": "24",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "RTX A4000",
+        "hourly-price": "0.61",
+        "VRAM": "16",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "Quadro RTX 5000",
+        "hourly-price": "0.57",
+        "VRAM": "16",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "Quadro RTX 4000",
+        "hourly-price": "0.24",
+        "VRAM": "8",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "Tesla V100 NVLINK",
+        "hourly-price": "0.80",
+        "VRAM": "16",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "NVIDIA H100",
+        "hourly-price": "2.10",
+        "VRAM": "320",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "NVIDIA A100",
+        "hourly-price": "1.89",
+        "VRAM": "80",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "NVIDIA L40S",
+        "hourly-price": "0.85",
+        "VRAM": "48",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "NVIDIA Quadro RTX A6000",
+        "hourly-price": "0.74",
+        "VRAM": "48",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "NVIDIA L4",
+        "hourly-price": "0.38",
+        "VRAM": "24",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "NVIDIA Quadro RTX 6000",
+        "hourly-price": "0.64",
+        "VRAM": "24",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "NVIDIA A30",
+        "hourly-price": "0.64",
+        "VRAM": "24",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    }
+]
