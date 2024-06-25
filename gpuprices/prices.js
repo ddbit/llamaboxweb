dbf692e3c383617e7c29c410816808285d07910d
davide <davide@Mac-mini-di-davide.local>
2024-06-25 10:18:19 +0200

    db update

--- gpuprices/prices.js
+++ gpuprices/prices.js
@@ -1,1 +1,410 @@
-[{"GPU": "H100", "hourly-price": "3.24", "VRAM": "80", "provider": "ori.co", "link": "https://www.ori.co/get-started"}, {"GPU": "A100", "hourly-price": "2.74", "VRAM": "80", "provider": "ori.co", "link": "https://www.ori.co/get-started"}, {"GPU": "V100", "hourly-price": "0.83", "VRAM": "16", "provider": "ori.co", "link": "https://www.ori.co/get-started"}, {"GPU": "A100", "hourly-price": 4.14, "VRAM": 40, "provider": "replicate", "link": "https://replicate.com/pricing"}, {"GPU": "A100", "hourly-price": 5.04, "VRAM": 80, "provider": "replicate", "link": "https://replicate.com/pricing"}, {"GPU": "A40", "hourly-price": 2.61, "VRAM": 48, "provider": "replicate", "link": "https://replicate.com/pricing"}, {"GPU": "A40", "hourly-price": 2.07, "VRAM": 48, "provider": "replicate", "link": "https://replicate.com/pricing"}, {"GPU": "T4", "hourly-price": 0.81, "VRAM": 16, "provider": "replicate", "link": "https://replicate.com/pricing"}, {"GPU": "H100", "hourly-price": "2.49", "VRAM": "80", "provider": "lambda labs", "link": "https://lambdalabs.com/service/gpu-cloud"}, {"GPU": "A100", "hourly-price": "1.29", "VRAM": "40", "provider": "lambda labs", "link": "https://lambdalabs.com/service/gpu-cloud"}, {"GPU": "A10", "hourly-price": "0.75", "VRAM": "24", "provider": "lambda labs", "link": "https://lambdalabs.com/service/gpu-cloud"}, {"GPU": "RTX A6000", "hourly-price": "0.80", "VRAM": "48", "provider": "lambda labs", "link": "https://lambdalabs.com/service/gpu-cloud"}, {"GPU": "V100", "hourly-price": "4.40", "VRAM": "16", "provider": "lambda labs", "link": "https://lambdalabs.com/service/gpu-cloud"}, {"GPU": "Quadro RTX 8000", "hourly-price": "0.50", "VRAM": "48", "provider": "lambda labs", "link": "https://lambdalabs.com/service/gpu-cloud"}, {"GPU": "H100", "hourly-price": "4.76", "VRAM": "80", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "H100", "hourly-price": "4.25", "VRAM": "80", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "A100", "hourly-price": "2.21", "VRAM": "80", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "A100", "hourly-price": "2.21", "VRAM": "80", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "A100", "hourly-price": "2.06", "VRAM": "40", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "A100", "hourly-price": "2.06", "VRAM": "40", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "A40", "hourly-price": "1.28", "VRAM": "48", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "RTX A6000", "hourly-price": "1.28", "VRAM": "48", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "V100", "hourly-price": "0.80", "VRAM": "16", "provider": "coreweave", "link": "https://www.coreweave.com/gpu-cloud-pricing"}, {"GPU": "H100", "hourly-price": "2.10", "VRAM": "80", "provider": "seeweb", "link": "https://www.seeweb.it/en/products/cloud-server-gpu"}, {"GPU": "A100", "hourly-price": "1.89", "VRAM": "80", "provider": "seeweb", "link": "https://www.seeweb.it/en/products/cloud-server-gpu"}, {"GPU": "RTX A6000", "hourly-price": "0.74", "VRAM": "48", "provider": "seeweb", "link": "https://www.seeweb.it/en/products/cloud-server-gpu"}, {"GPU": "A30", "hourly-price": "0.29", "VRAM": "24", "provider": "seeweb", "link": "https://www.seeweb.it/en/products/cloud-server-gpu"}, {"GPU": "H100", "hourly-price": "3.19", "VRAM": "80", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "H100", "hourly-price": "3.69", "VRAM": "80", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "H100", "hourly-price": "3.89", "VRAM": "80", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "H100", "hourly-price": "4.69", "VRAM": "80", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A100", "hourly-price": "1.49", "VRAM": "80", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A100", "hourly-price": "1.89", "VRAM": "80", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A100", "hourly-price": "2.19", "VRAM": "80", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A40", "hourly-price": "0.67", "VRAM": "48", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A40", "hourly-price": "0.69", "VRAM": "48", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A30", "hourly-price": "0.26", "VRAM": "24", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "RTX A6000", "hourly-price": "0.69", "VRAM": "48", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "RTX A6000", "hourly-price": "0.79", "VRAM": "48", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A5000", "hourly-price": "0.26", "VRAM": "24", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A5000", "hourly-price": "0.44", "VRAM": "24", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "RTX 4090", "hourly-price": "0.54", "VRAM": "24", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "RTX 4090", "hourly-price": "0.74", "VRAM": "24", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "RTX 3090", "hourly-price": "0.26", "VRAM": "24", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "RTX 3090", "hourly-price": "0.44", "VRAM": "24", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "RTX 3090 Ti", "hourly-price": "0.29", "VRAM": "24", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}, {"GPU": "A4500", "hourly-price": "0.21", "VRAM": "20", "provider": "runpod", "link": "https://www.runpod.io/gpu-instance/pricing"}]
+[
+    {
+        "GPU": "H100",
+        "Maker": "NVIDIA",
+        "hourly-price": 3.24,
+        "VRAM": 80,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": 2.74,
+        "VRAM": 80,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "V100",
+        "Maker": "NVIDIA",
+        "hourly-price": 0.83,
+        "VRAM": 16,
+        "provider": "ori.co",
+        "link": "https://www.ori.co/get-started"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": 5.04,
+        "VRAM": 80,
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "A40",
+        "Maker": "NVIDIA",
+        "hourly-price": 2.61,
+        "VRAM": 48,
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "A40",
+        "Maker": "NVIDIA",
+        "hourly-price": 2.07,
+        "VRAM": 48,
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "T4",
+        "Maker": "NVIDIA",
+        "hourly-price": 0.81,
+        "VRAM": 16,
+        "provider": "replicate",
+        "link": "https://replicate.com/pricing"
+    },
+    {
+        "GPU": "H100",
+        "Maker": "NVIDIA",
+        "hourly-price": 27.92,
+        "VRAM": 80,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "H100",
+        "Maker": "NVIDIA",
+        "hourly-price": 2.49,
+        "VRAM": 80,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": 14.32,
+        "VRAM": 80,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": 10.32,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": 1.29,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": 5.16,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": 2.58,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": 1.29,
+        "VRAM": 40,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "A10",
+        "Maker": "NVIDIA",
+        "hourly-price": 0.75,
+        "VRAM": 24,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "RTX A6000",
+        "Maker": "NVIDIA",
+        "hourly-price": 3.2,
+        "VRAM": 48,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "RTX A6000",
+        "Maker": "NVIDIA",
+        "hourly-price": 1.6,
+        "VRAM": 48,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "RTX A6000",
+        "Maker": "NVIDIA",
+        "hourly-price": 0.8,
+        "VRAM": 48,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "V100",
+        "Maker": "NVIDIA",
+        "hourly-price": 4.4,
+        "VRAM": 16,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "Quadro RTX 8000",
+        "Maker": "NVIDIA",
+        "hourly-price": 0.5,
+        "VRAM": 48,
+        "provider": "lambda labs",
+        "link": "https://lambdalabs.com/service/gpu-cloud"
+    },
+    {
+        "GPU": "H100",
+        "Maker": "NVIDIA",
+        "hourly-price": "4.76",
+        "VRAM": "80",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "H100",
+        "Maker": "NVIDIA",
+        "hourly-price": "4.25",
+        "VRAM": "80",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": "2.21",
+        "VRAM": "80",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": "2.21",
+        "VRAM": "80",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": "2.06",
+        "VRAM": "40",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": "2.06",
+        "VRAM": "40",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "A40",
+        "Maker": "NVIDIA",
+        "hourly-price": "1.28",
+        "VRAM": "48",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "RTX A6000",
+        "Maker": "NVIDIA",
+        "hourly-price": "1.28",
+        "VRAM": "48",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "RTX A5000",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.77",
+        "VRAM": "24",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "RTX A4000",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.61",
+        "VRAM": "16",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "Quadro RTX 5000",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.57",
+        "VRAM": "16",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "Quadro RTX 4000",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.24",
+        "VRAM": "8",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "V100",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.80",
+        "VRAM": "16",
+        "provider": "coreweave",
+        "link": "https://www.coreweave.com/gpu-cloud-pricing"
+    },
+    {
+        "GPU": "H100",
+        "Maker": "NVIDIA",
+        "hourly-price": "2.10",
+        "VRAM": "80",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": "1.89",
+        "VRAM": "80",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "RTX A6000",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.74",
+        "VRAM": "48",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "A30",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.29",
+        "VRAM": "24",
+        "provider": "seeweb",
+        "link": "https://www.seeweb.it/en/products/cloud-server-gpu"
+    },
+    {
+        "GPU": "H100",
+        "Maker": "NVIDIA",
+        "hourly-price": "3.19",
+        "VRAM": "80",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "H100",
+        "Maker": "NVIDIA",
+        "hourly-price": "3.89",
+        "VRAM": "80",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": "1.49",
+        "VRAM": "80",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": "2.19",
+        "VRAM": "80",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "A100",
+        "Maker": "NVIDIA",
+        "hourly-price": "4.89",
+        "VRAM": "80",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "A40",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.67",
+        "VRAM": "48",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "L40",
+        "Maker": "NVIDIA",
+        "hourly-price": "1.14",
+        "VRAM": "48",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "L40S",
+        "Maker": "NVIDIA",
+        "hourly-price": "1.09",
+        "VRAM": "48",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "RTX A6000",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.69",
+        "VRAM": "48",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "RTX A5000",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.26",
+        "VRAM": "24",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "RTX 4090",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.54",
+        "VRAM": "24",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "RTX 3090",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.26",
+        "VRAM": "24",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    },
+    {
+        "GPU": "A30",
+        "Maker": "NVIDIA",
+        "hourly-price": "0.26",
+        "VRAM": "24",
+        "provider": "runpod",
+        "link": "https://www.runpod.io/gpu-instance/pricing"
+    }
+]
